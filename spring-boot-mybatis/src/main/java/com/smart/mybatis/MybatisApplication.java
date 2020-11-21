d1db7e48902d1187a169d69d7e99dd69c51c0afc
ybq <hello@ybq.com>
2020-11-21 17:03:52 +0800

    Initial commit

---
+++ spring-boot-mybatis/src/main/java/com/smart/mybatis/MybatisApplication.java
@@ -0,0 +1,14 @@
+package com.smart.mybatis;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//扫描Mapper接口
+@MapperScan(value = "com.smart.mybatis.mapper")
+public class MybatisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisApplication.class, args);
+    }
+}
